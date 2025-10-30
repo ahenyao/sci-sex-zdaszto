314349961615b23a8aa43847a5a8bad1b0ed2b0a
AlanKajkowskiSCI <alankajkowski@microsoft.sci.edu.pl>
2025-10-30 21:20:52 +0100

    Utworzenie AddAccountView.axaml i drobne poprawki w LoginView.axaml
    Utworzenie AddAccountView.axaml i AddAccountViewModel.cs, poprawki w LoginView.axaml

---
+++ ZdaszToApp/ZdaszToApp/Views/AddAccountView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ZdaszToApp.Views;
+
+public partial class AddAccountView : UserControl
+{
+    public AddAccountView()
+    {
+        InitializeComponent();
+    }
+}
