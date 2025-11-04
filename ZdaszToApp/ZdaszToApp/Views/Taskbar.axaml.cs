fb4faa2727c7ded100be068071250203ae0916c9
AlanKajkowskiSCI <alankajkowski@microsoft.sci.edu.pl>
2025-11-04 21:56:06 +0100

    Dodanie grafik dla głównego menu i stworzenie layoutu głównego menu
    Dodanie 3 grafik dla poszczególnych egzaminów zawodowych, utworzenie MenuView.axaml (axaml kompletny, bez code-behind) i Taskbar.axaml (do zrobienia)

---
+++ ZdaszToApp/ZdaszToApp/Views/Taskbar.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ZdaszToApp.Views;
+
+public partial class Taskbar : UserControl
+{
+    public Taskbar()
+    {
+        InitializeComponent();
+    }
+}
