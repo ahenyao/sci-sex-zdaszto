a00756d672f2cce5b71fc2556aa7068b2260b6d6
AlanKajkowskiSCI <alankajkowski@microsoft.sci.edu.pl>
2025-12-11 21:51:03 +0100

    zmiana okien przy logowaniu
    Próbowałem, ale nadal są problemy

--- ZdaszToApp/ZdaszToApp/App.axaml.cs
+++ ZdaszToApp/ZdaszToApp/App.axaml.cs
@@ -23,9 +23,9 @@ public partial class App : Application
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
-            desktop.MainWindow = new MainWindow
+            desktop.MainWindow = new LoginWindow
             {
-                DataContext = new MainViewModel()
+                DataContext = new LoginViewModel()
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
--- ZdaszToApp/ZdaszToApp/ViewModels/LoginViewModel.cs
+++ ZdaszToApp/ZdaszToApp/ViewModels/LoginViewModel.cs
@@ -3,16 +3,23 @@ using CommunityToolkit.Mvvm.Input;
 using System.Collections.Generic;
 using System.Linq;
 using Tmds.DBus.Protocol;
+using System;
 
 namespace ZdaszToApp.ViewModels;
 
 public partial class LoginViewModel : ViewModelBase
 {
-    [ObservableProperty] private string username;
+    [ObservableProperty] private string? username;
 
-    [ObservableProperty] private string password;
+    [ObservableProperty] private string? password;
 
-    [ObservableProperty] private string message;
+    [ObservableProperty] private string? message;
+
+    [ObservableProperty] private string? error_login = "Wpisz login";
+    
+    [ObservableProperty] private string? error_password = "Wpisz hasło";
+    
+    [ObservableProperty] private bool isLoggedIn;
 
     private static List<(string Username, string Password)> Users = new()
     {
@@ -23,6 +30,7 @@ public partial class LoginViewModel : ViewModelBase
     [RelayCommand]
     private void Login()
     {
+        
         if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password))
         {
             Message = "Nazwa użytkownika i hasło nie mogą być puste.";
@@ -30,14 +38,31 @@ public partial class LoginViewModel : ViewModelBase
         }
 
         var user = Users.FirstOrDefault(u => u.Username == Username && u.Password == Password);
-
+        
         if (user == default)
         {
             Message = "Nieprawidłowa nazwa użytkownika lub hasło.";
+            Password = string.Empty;
+            Username = string.Empty;
         }
         else
         {
             Message = "Logowanie udane!";
+            IsLoggedIn = true;
+        }
+        
+        var userByLogin = Users.FirstOrDefault(u => u.Username == Username);
+        if (userByLogin == default)
+        {
+            Error_login = "Wpisany login jest błędny";
+        }
+        else
+        {
+            if (userByLogin.Password != Password)
+            {
+                Error_password = "Wpisane hasło jest błędne";
+            }
+            
         }
     }
 }
